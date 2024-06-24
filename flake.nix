91c1dcf4c7af9d228586ea25514ed757c6f3bc4f
Seungheon Oh <seungheon.ooh@gmail.com>
2024-06-23 19:55:23 -0500

    Use latest lem that doesn't require patching for quicklisp
    SDL also works

--- flake.nix
+++ flake.nix
@@ -1,54 +1,79 @@
 {
-  description = "A very basic flake";
+  description = "lem";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      cl-charms = pkgs.sbclPackages.cl-charms.overrideLispAttrs
-        (oldAttrs: { nativeLibs = [ pkgs.ncurses ]; });
-      jsonrpc = pkgs.sbclPackages.jsonrpc.overrideLispAttrs (oldAttrs: {
-        src = pkgs.fetchFromGitHub {
-          owner = "cxxxr";
-          repo = "jsonrpc";
-          rev = "6e3d23f9bec1af1a3155c21cc05dad9d856754bc";
-          hash = "sha256-QbXesQbHHrDtcV2D4FTnKMacEYZJb2mRBIMC7hZM/A8=";
-        };
-        systems =
-          [ "jsonrpc" "jsonrpc/transport/stdio" "jsonrpc/transport/tcp" ];
-        lispLibs = with pkgs.sbclPackages;
-          oldAttrs.lispLibs ++ [ cl_plus_ssl quri fast-io trivial-utf-8 ];
-      });
-      queues = pkgs.sbclPackages.queues.overrideLispAttrs (oldAttrs: {
-        systems = [ "queues" "queues.priority-cqueue" "queues.priority-queue" "queues.simple-cqueue" "queues.simple-queue" ];
-        lispLibs = oldAttrs.lispLibs ++ (with pkgs.sbclPackages; [bordeaux-threads]);
-      });
-    in {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
 
-      packages.x86_64-linux.micros = pkgs.sbcl.buildASDFSystem {
-        pname = "micros";
-        version = "unstable-2024-05-15";
-        src = pkgs.fetchFromGitHub {
-          owner = "lem-project";
-          repo = "micros";
-          rev = "f80d7772ca76e9184d9bc96bc227147b429b11ed";
-          hash = "sha256-RiBHxKWVZsB4JPktLSVcup7WIUMk08VbxU1zeBfGrFQ=";
-        };
-        patches = [ ./micros.patch ];
-      };
-      
-      packages.x86_64-linux.lem-mailbox = pkgs.sbcl.buildASDFSystem {
-        pname = "lem-mailbox";
-        version = "unstable-2023-09-10";
-        src = pkgs.fetchFromGitHub {
-          owner = "lem-project";
-          repo = "lem-mailbox";
-          rev = "12d629541da440fadf771b0225a051ae65fa342a";
-          hash = "sha256-hb6GSWA7vUuvSSPSmfZ80aBuvSVyg74qveoCPRP2CeI=";
-        };
-        lispLibs = with pkgs.sbcl.pkgs; [ bordeaux-threads bt-semaphore queues ];
-      };
+  outputs = {self, nixpkgs}: {
+    packages.x86_64-linux.lem =
+      with import nixpkgs { system = "x86_64-linux"; };
 
-      packages.x86_64-linux.lem-base16-themes = pkgs.sbcl.buildASDFSystem {
+      let
+        micros = pkgs.sbcl.buildASDFSystem rec {
+          pname = "micros";
+          version = "0.0.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "lem-project";
+            repo = "micros";
+            rev = "9fc7f1e5b0dbf1b9218a3f0aca7ed46e90aa86fd";
+            sha256 = "sha256-bLFqFA3VxtS5qDEVVi1aTFYLZ33wsJUf26bwIY46Gtw=";
+          };
+        };
+
+        lem-mailbox = pkgs.sbcl.buildASDFSystem rec {
+          pname = "lem-mailbox";
+          version = "0.0.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "lem-project";
+            repo = "lem-mailbox";
+            rev = "12d629541da440fadf771b0225a051ae65fa342a";
+            sha256 = "sha256-hb6GSWA7vUuvSSPSmfZ80aBuvSVyg74qveoCPRP2CeI=";
+          };
+          lispLibs = with pkgs.sbclPackages; [
+            bordeaux-threads
+            bt-semaphore
+	          queues
+            queues_dot_simple-cqueue
+          ];
+        };
+
+      in
+        pkgs.sbcl.buildASDFSystem rec {
+          pname = "lem";
+
+          version = "2.2.0";
+
+          # https://nixos.wiki/wiki/Packaging/Binaries
+          src = pkgs.fetchFromGitHub {
+            owner = "lem-project";
+            repo = "lem";
+            rev = "2c6a0da38594d4eb2c28a0151326bc19994fecb9";
+            sha256 = "sha256-AuzN82m3+m+I3Qlmr3AqMVCI75XJSc3rELdJe+GxbZ4=";
+          };
+          lispLibs = with pkgs.sbclPackages; [
+            iterate
+            closer-mop
+            trivia
+            alexandria
+            trivial-gray-streams
+            trivial-types
+            cl-ppcre
+            inquisitor
+            babel
+            bordeaux-threads
+            yason
+            log4cl
+            split-sequence
+            str
+            dexador
+            qlot
+          ] ++ [micros lem-mailbox];
+        };
+
+    packages.x86_64-linux.lem-base16-themes =
+      with import nixpkgs { system = "x86_64-linux"; };
+      pkgs.sbcl.buildASDFSystem {
         pname = "lem-base16-themes";
         version = "unstable-2023-07-04";
         src = pkgs.fetchFromGitHub {
@@ -60,14 +85,47 @@
         lispLibs = [self.packages.x86_64-linux.lem];
       };
 
-      
-      packages.x86_64-linux.lem = pkgs.callPackage ./default.nix { inherit cl-charms jsonrpc queues; micros = self.packages.x86_64-linux.micros; lem-mailbox = self.packages.x86_64-linux.lem-mailbox; lem = self.packages.x86_64-linux.lem; };
-      packages.x86_64-linux.lem-exec = frontend: pkgs.sbcl.buildASDFSystem {
+    packages.x86_64-linux.lem-exec =
+      with import nixpkgs { system = "x86_64-linux"; };
+      let
+        jsonrpc = pkgs.sbclPackages.jsonrpc.overrideLispAttrs (oldAttrs: {
+          src = pkgs.fetchFromGitHub {
+            owner = "cxxxr";
+            repo = "jsonrpc";
+            rev = "6e3d23f9bec1af1a3155c21cc05dad9d856754bc";
+            hash = "sha256-QbXesQbHHrDtcV2D4FTnKMacEYZJb2mRBIMC7hZM/A8=";
+          };
+          systems =
+            [ "jsonrpc" "jsonrpc/transport/stdio" "jsonrpc/transport/tcp" ];
+          lispLibs = with pkgs.sbclPackages;
+            oldAttrs.lispLibs ++ [ cl_plus_ssl quri fast-io trivial-utf-8 ];
+        });
+        queues = pkgs.sbclPackages.queues.overrideLispAttrs (oldAttrs: {
+          systems = [ "queues" "queues.priority-cqueue" "queues.priority-queue" "queues.simple-cqueue" "queues.simple-queue" ];
+          lispLibs = oldAttrs.lispLibs ++ (with pkgs.sbclPackages; [bordeaux-threads]);
+        });
+      in
+      frontend: pkgs.sbcl.buildASDFSystem {
         inherit (self.packages.x86_64-linux.lem) src;
         pname = "lem-exec";
         version = "unstable";
-        lispLibs = [self.packages.x86_64-linux.lem self.packages.x86_64-linux.lem-base16-themes jsonrpc cl-charms]
-        ++ (with pkgs.sbcl.pkgs; [_3bmd _3bmd-ext-code-blocks lisp-preprocessor trivial-ws trivial-open-browser])
+        lispLibs = [
+          self.packages.x86_64-linux.lem
+          self.packages.x86_64-linux.lem-base16-themes
+          jsonrpc
+        ] ++ (with pkgs.sbcl.pkgs; [
+          _3bmd
+          _3bmd-ext-code-blocks
+          lisp-preprocessor
+          trivial-ws
+          trivial-open-browser
+          cl-charms
+          cl-setlocale
+          parse-number
+          cl-package-locks
+          async-process
+          swank
+        ])
         ++ (if frontend == "sdl2" then (with pkgs.sbcl.pkgs; [sdl2 sdl2-ttf sdl2-image trivial-main-thread]) else []);
         nativeLibs = if frontend == "sdl2" then with pkgs; [SDL2 SDL2_ttf SDL2_image] else [];
         nativeBuildInputs = with pkgs; [ openssl makeWrapper ];
@@ -84,7 +142,6 @@
             #+sb-core-compression t
             :toplevel #'lem:main)
         '';
-        patches = [ ./fix-quickload.patch ];
         installPhase = ''
           mkdir -p $out/bin
           cp -v lem $out/bin
@@ -96,22 +153,7 @@
         };
       };
 
-      packages.x86_64-linux.lem-ncurses = self.packages.x86_64-linux.lem-exec "ncurses";
-      packages.x86_64-linux.lem-sdl2 = self.packages.x86_64-linux.lem-exec "sdl2";
-      packages.x86_64-linux.default = self.packages.x86_64-linux.lem-ncurses;
-
-      overlays.default = final: prev: {lem-ncurses = self.packages.x86_64-linux.lem-ncurses;
-                                       lem-sdl2 = self.packages.x86_64-linux.lem-sdl2;};
-
-      devShells.x86_64-linux.default =
-        let
-          sbcl' = pkgs.sbcl.withPackages (ps: with ps; [cl-cram]);
-        in
-       pkgs.mkShell {
-
-        #CL_SOURCE_REGISTRY = pkgs.lib.makeSearchPath "" (with pkgs.sbclPackages; [ cl-cram ]);
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl pkgs.ncurses pkgs.libffi pkgs.SDL2 pkgs.SDL2_ttf pkgs.SDL2_image pkgs.tree-sitter-grammars.tree-sitter-c];
-        buildInputs = [pkgs.openssl pkgs.ncurses pkgs.roswell pkgs.SDL2 pkgs.SDL2_ttf pkgs.SDL2_image pkgs.pkg-config pkgs.libffi pkgs.tree-sitter-grammars.tree-sitter-c sbcl'];
-      };
-    };
+    packages.x86_64-linux.lem-ncurses = self.packages.x86_64-linux.lem-exec "ncurses";
+    packages.x86_64-linux.lem-sdl2 = self.packages.x86_64-linux.lem-exec "sdl2";
+  };
 }
